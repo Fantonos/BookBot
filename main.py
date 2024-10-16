b2b6485465cdca28a2fe8307e8b523cffca95d44
Fantonos <galaxxy@sudomail.com>
2024-10-15 21:36:36 -0400

    added word count

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_of_words(book_path)
+    #print(text)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def num_of_words(path):
+    word_count = len((get_book_text(path)).split())
+    print(word_count)
 
 
 main()
